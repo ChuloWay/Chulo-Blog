e81c9a23faa3272095d3c58da29b7fc994947d9a
Okoye Victor <okoyeebuka64@gmail.com>
2022-03-18 13:01:19 +0100

    Initialized templating Engine

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+//const res = require('express/lib/response');
 const app = express();
+const path = require('path');
+const ejs = require('ejs');
+app.set('view engine','ejs');
 
-app.listen(4000,()=>{
+app.use(express.static('public'))
+
+app.listen(4000, () => {
     console.log('App Listening On Port 4000!!');
+})
+
+app.get('/', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
+})
+app.get('/post', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
+})
+app.get('/contact', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
+
 })
