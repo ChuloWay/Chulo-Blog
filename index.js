69c98fd19e0d20cd03a786032cfd042499119316
Okoye Victor <okoyeebuka64@gmail.com>
2022-03-28 15:45:46 +0100

    updated page route

--- index.js
+++ index.js
@@ -25,16 +25,20 @@ app.get('/', async(req, res) => {
     res.render('index',{
         blogposts
     })
-    console.log(blogposts);
+    // console.log(blogposts);
 })
 app.get('/about', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
     res.render('about')
 })
-app.get('/post', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
-    res.render('post')
+app.get('/post/:id',async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id)
+   
+    res.render('post',{
+        blogpost
+    })
 })
+
 app.get('/contact', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
     res.render('contact')
