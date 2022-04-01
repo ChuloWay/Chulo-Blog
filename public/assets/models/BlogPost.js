ecdff8a56ec9e9e00a892dd3a8aa7d03fda65df9
Okoye Victor <okoyeebuka64@gmail.com>
2022-04-01 17:26:10 +0100

    new feature

--- public/assets/models/BlogPost.js
+++ public/assets/models/BlogPost.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
-const BlogPostSchema= new schema({
-    title:String,
-    body:String
+const BlogPostSchema = new schema({
+    title: String,
+    body: String,
+    username: String,
+    datePosted: {/*can declare*/
+        type: Date,
+        default: new Date()
+    }
 });
 
-const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 
 module.exports = BlogPost;
