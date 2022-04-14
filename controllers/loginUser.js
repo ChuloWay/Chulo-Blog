acf7b54cef832433772fa594661b0c84d9fecdaf
Okoye Victor <okoyeebuka64@gmail.com>
2022-04-14 22:05:27 +0100

    ...

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -18,6 +18,7 @@ module.exports = (req,res) =>{
             })
         }
         else{
+            errors: req.flash('validationErrors')
             console.log("/auth/login::",user)
             res.redirect('/auth/login')
         }
